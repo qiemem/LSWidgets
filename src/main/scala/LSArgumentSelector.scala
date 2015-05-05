d88a6806f5770f9df0ee3e8ff4ce4725ae543c9c
Bryan Head <bryan.head@gmail.com>
2015-05-05 17:16:21 -0500

    Preserve argument order

--- src/main/scala/LSArgumentSelector.scala
+++ src/main/scala/LSArgumentSelector.scala
@@ -13,9 +13,11 @@ import org.nlogo.window.GUIWorkspace
 
 import uk.ac.surrey.xw.api.swing.enrichItemSelectable
 
+import scala.collection.immutable.ListMap
+
 class LSArgumentSelector(changeCallback: ()=>Unit, ws: GUIWorkspace, maxTextLength: Option[Int]) extends JPanel {
   import org.levelspace.Enhancer._
-  private var arguments = Map.empty[String, XWComboBox]
+  private var arguments = ListMap.empty[String, XWComboBox]
 
   def selectedArguments: LogoList = selectedArgumentsLogoList(arguments)
   def selectedArguments_=(args: LogoList): Unit = setSelectedArguments(arguments, args)
@@ -38,17 +40,17 @@ class LSArgumentSelector(changeCallback: ()=>Unit, ws: GUIWorkspace, maxTextLeng
     layoutArgumentPanel(arguments)
   }
 
-  private def selectedArgumentsLogoList(args: Map[String, XWComboBox]): LogoList =
+  private def selectedArgumentsLogoList(args: ListMap[String, XWComboBox]): LogoList =
     args.map {
       case (name: String, comboBox: XWComboBox) => LogoList(name, comboBox.selectedItem)
     }.toSeq.toLogo
 
-  private def availableArgumentsLogoList(args: Map[String, XWComboBox]): LogoList =
+  private def availableArgumentsLogoList(args: ListMap[String, XWComboBox]): LogoList =
     args.map {
       case (name: String, comboBox: XWComboBox) => LogoList(name, comboBox.items.toLogo)
     }.toSeq.toLogo
 
-  private def setSelectedArguments(args: Map[String, XWComboBox], list: LogoList): Unit = list.foreach {
+  private def setSelectedArguments(args: ListMap[String, XWComboBox], list: LogoList): Unit = list.foreach {
     case arg: LogoList =>
       val name = arg.get(0).asInstanceOf[String]
       val value = arg.get(1).asInstanceOf[String]
@@ -56,19 +58,19 @@ class LSArgumentSelector(changeCallback: ()=>Unit, ws: GUIWorkspace, maxTextLeng
     case x => ws.warningMessage("Invalid selection item: " + x.toString)
   }
 
-  private def assembleArguments(argumentList: LogoList): Map[String, XWComboBox] = {
+  private def assembleArguments(argumentList: LogoList): ListMap[String, XWComboBox] = {
     val (names, items) = argumentList.map(_.asInstanceOf[LogoList]).map { l =>
       l.get(0).toString -> l.get(1).asInstanceOf[LogoList].map(_.toString)
     }.toSeq.unzip
-    names.zip(items.map {
+    ListMap(names.zip(items.map {
       opts =>
         val chooser: XWComboBox = new XWComboBox(changeCallback, maxChars = maxTextLength)
         chooser.items = opts.toSeq
         chooser
-    }).toMap
+    }): _*)
   }
 
-  private def layoutArgumentPanel[T <: ExtraWidget, S](arguments: Map[String, XWComboBox]) = {
+  private def layoutArgumentPanel[T <: ExtraWidget, S](arguments: ListMap[String, XWComboBox]) = {
     removeAll()
     setLayout(new MigLayout("insets 0"))
     arguments.foreach {
